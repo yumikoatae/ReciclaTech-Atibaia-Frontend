5362ac69bb823e4a086bc1ed7ba6b109bd0e8176
Yumiko Atae <dev.atae02@gmail.com>
2025-07-07 21:17:23 -0300

    style: melhora aparência da página inicial com ajustes em Home e App.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,11 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>Bem-vindo ao <span className="highlight">ReciclaTech ♻️</span></h1>
-      <p>Conectando pessoas, tecnologia e o meio ambiente para um futuro mais sustentável.</p>
+      <h1>Conheça a <span className="highlight">ReciclaTech Atibaia</span></h1>
+      <p>
+        Bem-vindo ao ReciclaTech, o seu parceiro em Atibaia para o descarte responsável de resíduos eletrônicos.
+        Nosso compromisso é promover práticas sustentáveis no descarte de lixo eletrônico, contribuindo para um ambiente mais limpo e sustentável.
+      </p>
       <img
         src="https://cdn-icons-png.flaticon.com/512/4305/4305733.png"
         alt="Ilustração sustentável"
